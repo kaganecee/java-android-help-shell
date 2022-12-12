ffeb4ec5424c6fa1853413eb44221c752c87384a
kaganecee <kaganecee@gmail.com>
2022-12-12 20:38:50 +0300

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button books = findViewById(R.id.books);
+        Button youtubebutton = findViewById(R.id.youtubebutton);
+        Button websitesButton = findViewById(R.id.websitesbutton);
+        Button githubreposButton = findViewById(R.id.githubreposButton);
+
+        books.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Books());
+            }
+        });
+        books.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new YoutubeChannels());
+            }
+        });
+
+        Button loginButton = (Button) findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(this,loginActivity.class);
+                intent.putExtra("msg","hello");
+                startActivity(intent);
+            }
+        });
+    }
+    private void replaceFragment (Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment bookFragment = fm.findFragmentById("books");
+        fm.beginTransaction().replace(R.id.containerView,bookFragment).commit();
+    }
+}
