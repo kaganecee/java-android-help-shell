7050da92f75d0d29ba80f8d108163965ceebe359
kaganece <kaganece@jotform.com>
2022-12-15 20:54:10 +0300

    feat(profile): profile page activity added

---
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProfileActivity extends AppCompatActivity {
+    EditText name = findViewById(R.id.name);
+    EditText surname = findViewById(R.id.surname);
+    EditText password = findViewById(R.id.password);
+    EditText confirmPassword = findViewById(R.id.confirmPassword);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_list);
+    }
+    public void addNewTask(View v){
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("name", name);
+        docData.put("surname", taskPriority);
+        docData.put("status", taskStatus);
+        docData.put("deadline", taskDeadline);
+        db.collection("data").set(docData);
+    }
+    public object getTask(View v){
+        Array tasks = db.collection("tasks").get();
+    }
+    public void openActivity (Class activityClass){
+        Intent intent = new Intent(this,activityClass);
+        startActivity(intent);
+    }
+}
