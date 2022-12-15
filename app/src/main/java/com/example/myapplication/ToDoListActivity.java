24468e0c22dde6e965ed2d48b8a7532423a32f8f
kaganece <kaganece@jotform.com>
2022-12-15 21:00:13 +0300

    fix(todolist): add new item func updated

--- app/src/main/java/com/example/myapplication/ToDoListActivity.java
+++ app/src/main/java/com/example/myapplication/ToDoListActivity.java
@@ -8,15 +8,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ToDoListActivity extends AppCompatActivity {
-    EditText taskInput = findViewById(R.id.books);
+    EditText taskName = findViewById(R.id.books);
+    EditText taskPriority = findViewById(R.id.books);
+    EditText taskStatus = findViewById(R.id.books);
+    EditText taskDeadline = findViewById(R.id.books);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
     }
-    public void addItemToList(View v){
-        toDoList.add(taskInput.getText().toString());
+    public void addNewTask(View v){
+
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("name", taskName);
+        docData.put("priority", taskPriority);
+        docData.put("status", taskStatus);
+        docData.put("deadline", taskDeadline);
+        db.collection("data").add(docData);
+    }
+    public object getTask(View v){
+        Array tasks = db.collection("tasks").get();
     }
     public void openActivity (Class activityClass){
         Intent intent = new Intent(this,activityClass);
