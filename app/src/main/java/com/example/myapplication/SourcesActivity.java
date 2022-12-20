3c8a9c603bd8287d3ce5dae2225393f2ecd88d6b
kaganecee <kaganecee@gmail.com>
2022-12-20 21:25:41 +0300

    fix(sources): fragments working issue fixed

--- app/src/main/java/com/example/myapplication/SourcesActivity.java
+++ app/src/main/java/com/example/myapplication/SourcesActivity.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +17,7 @@ public class SourcesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sources);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button books = findViewById(R.id.books);
         Button youtubebutton = findViewById(R.id.youtubebutton);
@@ -24,32 +27,54 @@ public class SourcesActivity extends AppCompatActivity {
         books.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                replaceFragment(new Books());
+                replaceFragment(new Books(), R.id.booksContainer);
             }
         });
         youtubebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                replaceFragment(new YoutubeChannels());
+                replaceFragment(new YoutubeChannels(), R.id.youtubeContainer);
             }
         });
         websitesButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {replaceFragment(new Websites());
+            public void onClick(View view) {replaceFragment(new Websites(), R.id.websitesContainer);
             }
         });
 
         githubreposButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {replaceFragment(new GithubRepos());
+            public void onClick(View view) {replaceFragment(new GithubRepos(),R.id.githubContainer);
             }
         });
 
 
     }
-    private void replaceFragment (Fragment fragment){
+    public void changeVisibilityOfElements(){
+        findViewById(R.id.books).setVisibility(View.GONE);
+        findViewById(R.id.textView).setVisibility(View.GONE);
+        findViewById(R.id.textView3).setVisibility(View.GONE);
+        findViewById(R.id.youtubebutton).setVisibility(View.GONE);
+        findViewById(R.id.websitesbutton).setVisibility(View.GONE);
+        findViewById(R.id.githubreposButton).setVisibility(View.GONE);
+    }
+    private void replaceFragment (Fragment fragment, int containerId){
+        changeVisibilityOfElements();
         FragmentManager fm = getSupportFragmentManager();
-        Fragment bookFragment = fm.findFragmentById("books");
-        fm.beginTransaction().replace(R.id.containerView,bookFragment).commit();
+        fm.beginTransaction().replace(containerId,fragment).commit();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
