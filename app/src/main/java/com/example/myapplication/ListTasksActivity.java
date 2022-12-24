16ca96565d072b02e468d780a31455c4261a01c1
kaganece <kaganece@jotform.com>
2022-12-24 20:35:44 +0300

    fix(todolist): fixed database post and get methods

--- app/src/main/java/com/example/myapplication/ListTasksActivity.java
+++ app/src/main/java/com/example/myapplication/ListTasksActivity.java
@@ -1,16 +1,24 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.sql.Array;
 
@@ -30,9 +38,19 @@ public class ListTasksActivity extends AppCompatActivity {
             }
         });
     }
-//    public void getTask(View v){
-//        Array tasks = db.collection("tasks").get();
-//    }
+    public void getTask(View v){
+        Task tasks = db.collection("tasks").get();
+        tasks.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    Log.w(TAG,"Error getting documents.", task.getException());
+                }
+            }
 
     public void openActivity (){
         Intent intent = new Intent(this,ToDoListActivity.class);
