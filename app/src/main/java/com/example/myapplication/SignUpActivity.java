a01a6e76c9a9a47888ce23771822649307c4fcd1
kaganecee <kaganecee@gmail.com>
2022-12-26 21:09:35 +0300

    fix(signUpActivity): fixed variable scope issue

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -6,15 +6,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -27,6 +24,7 @@ import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Boolean isUserExist = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,26 +42,24 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
     public void handleNewUsers(String username, String email, String password) {
-        Boolean isUserExist = findUserByUsername(username);
+        Boolean isUserExist = isUserExist(username);
         if(!isUserExist){
             Map<String, Object> userInfo = new HashMap<>();
             userInfo.put("username", username);
             userInfo.put("email", email);
             userInfo.put("password", password);
             db.collection("users").add(userInfo);
+        }else{
+            Toast.makeText(getApplicationContext(),"This username is already exist.",Toast.LENGTH_SHORT).show();
         }
-        // Make toast message
     }
     public void clearInputs(EditText username, EditText email, EditText password){
         username.getText().clear();
         email.getText().clear();
         password.getText().clear();
     }
-    public boolean findUserByUsername(String username){
-        // Must be user type
+    public boolean isUserExist(String username){
         Task users = db.collection("users").get();
-        Boolean isUserExist = false;
-        // Scope issue of variable
         users.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
