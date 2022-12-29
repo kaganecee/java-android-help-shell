859b1d8e695ac54a6b671328e0bd7bcffa2578f7
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2022-12-29 20:12:35 +0300

    feat(navigation): navigation added as a prototype

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,37 @@ public class MainActivity extends AppCompatActivity {
         Button timerButton = findViewById(R.id.timerButton);
         Button toDoListButton = findViewById(R.id.toDoListButton);
         Button sourcesButton = findViewById(R.id.sourcesButton);
+
+
+        bottomNavigationView.findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this,"Home", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.timer:
+                        Toast.makeText(MainActivity.this,"Timer", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.todolist:
+                        Toast.makeText(MainActivity.this,"To-Do List", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.sources:
+                        Toast.makeText(MainActivity.this,"Sources", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.roadmaps:
+                        Toast.makeText(MainActivity.this,"Roadmaps", Toast.LENGTH_LONG).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+
+
 
         roadmapsButton.setOnClickListener(new View.OnClickListener() {
             @Override
